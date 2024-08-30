a039699ce499df49faf573bde0c9c78cb27c2852
Rohitsah6372 <91113994+Rohitsah6372@users.noreply.github.com>
2024-08-30 09:54:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
+
+	arr.sort((a,b)=> a-b);
+	let cost =0;
+
+	while(arr.length >1){
+		let first = arr.shift(); 
+		let second = arr.shift(); 
+		cost += first + second;
+		arr.push(first + second);
+		arr.sort((a,b)=> a-b);
+	}
+	return cost;
   
 }
 
